4829aa0de17d1d86b281a182312240c35733ee7b
Luiza Ogura <luizapogura@gmail.com>
2022-10-21 17:17:15 -0300

    render list of planets in solarsystem

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage} = this.props;
+    const frase = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ frase } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.defaultProps = {
+  planetName: '',
+  planetImage: '',
+};
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ planeta: nome, image }) => (
+          <PlanetCard key={ nome } planetName={ nome } planetImage={ image } />
+        ))}
       </div>
     );
   }
